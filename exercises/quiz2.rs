58ebc30d9400d0c58ecbb2f37710f47f284d0b0c
darko.deuric <darko.deuric@ethernal.tech>
2022-08-09 11:15:04 +0200

    Quiz2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,7 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // Execute `rustlings hint quiz2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -30,11 +29,28 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Append(mut u) => {
+                    let mut s = String::from(string);
+                    while u > 0 {
+                        s.push_str("bar");
+                        u=u-1;
+                    }
+                    output.push(s);
+                }
+                Command::Trim => {
+                    output.push(string.trim().to_string());  
+                }
+                Command::Uppercase => {
+                    output.push(string.to_uppercase());
+                }
+                
+            }
         }
         output
     }
@@ -43,12 +59,12 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What to we have to import to have `transformer` in scope?
-    use ???;
+    use my_module;
     use super::Command;
 
     #[test]
     fn it_works() {
-        let output = transformer(vec![
+        let output = my_module::transformer(vec![
             ("hello".into(), Command::Uppercase),
             (" all roads lead to rome! ".into(), Command::Trim),
             ("foo".into(), Command::Append(1)),
